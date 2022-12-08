38d0d53c2fde5c439e9c81ea83f6690d314ea281
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-08 14:28:37 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
@@ -15,6 +17,7 @@ int main(void)
 {
 
 		char test_str[] = "\"Programming is like building a multilingual puzzle";
+
 
 
 			puts(test_str);
