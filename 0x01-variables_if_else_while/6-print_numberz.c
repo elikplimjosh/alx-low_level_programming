c1cb2306a4a150cbf32bb306d62c4ac36a941517
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-09 15:16:45 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Numberz
+ *
+ * Return: Always: 0
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
