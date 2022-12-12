991fd56534335327bc4e72d0a573a9008de03c94
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-12 14:13:05 -0800

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,16 +9,12 @@
  */
 int main(void)
 {
-	int n, last_d;
-	srand(time(0\"n"));
-	n = rand() - RAND_MAX / 2;
-	last_d = n % 10;
-	if (last_d > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
-	else if (last_d == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_d);
-	else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 	return (0);
 }
 
