280b22c4833d20fd4b85269daede57a1cf73b37f
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-12 05:32:05 -0800

    Size is not grandeu

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
-
 #include <string.h>
-
-#include <unistd.h>
-
+#include <unistd.>
 /**
- *
- *  * main - main block
- *
- *   * Return: 1
- *
- *    */
-
+ * main - main block
+ *  Return: 1
+ */
 int main(void)
-
 {
-
-		int s;
-
-
-
-			s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-
-
-				write(2,
-
-							      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-
-							      	      s);
+	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+				
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+       	s);
 
 					return (1);
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,47 +1,20 @@
 #include <stdio.h>
-
-
-
 /**
+ *  main - Size not grandeur
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *  Return: 0
+ */
 int main(void)
-
 {
-
-
-
 		char c;
-
-			int d;
-
-				long int ld;
-
-					long long int lld;
-
-						float f;
-
-
-
-							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-
-									printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ld));
-
-										printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
-
-											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-
-
-												return (0);
-
+		int d;
+		long int ld;
+		long long int lld;
+		float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ld));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
