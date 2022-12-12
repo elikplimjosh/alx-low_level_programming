88e781aa201e316f16a2d0782e640fa81a64f737
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-12 13:09:48 -0800

    Unix is basically simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,11 @@
 /**
 * main - UNIX is basically simple
 *
-*  Return: 0
+*  Return: 1
 */
 int main(void)
 {
-	int s;
-
-	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 
 }
