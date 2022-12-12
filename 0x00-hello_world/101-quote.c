ac55d7d3e65f2eb7a14703a3623d798a97144919
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-12 08:34:59 -0800

    UNIX is basically simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,18 +2,16 @@
 #include <string.h>
 #include <unistd.>
 /**
- * main - main block
- *  Return: 1
- */
+* main - UNIX is basically simple
+*
+*  Return: 0
+*/
 int main(void)
 {
 	int s;
-	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-				
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-       	s);
 
-					return (1);
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	return (0);
 
 }
