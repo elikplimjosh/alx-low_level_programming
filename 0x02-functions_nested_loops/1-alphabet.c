9ddce2ee80930a2a5efe5bfbd25a4f9c46eb831a
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-14 07:59:04 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - print all alphabets in small case
+ *
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+		_putchar(letter);
+
+	_putchar('\n')
+}
