f8dc98f83c5dc92532e50ec6230b32a749877ec3
elikplimjosh <jayakakpo4u2@gmail.com>
2022-12-15 14:41:52 -0800

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - prints a number using _putchar
- * @n: number to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-	unsigned int un = 0;
-
-	if (n < 0)
-	{
-		if (n < 1000000000)
-			n = -n;
-		un = n;
-		_putchar(45);
-		num_to_char(n);
-	}
-	else
-	{
-		un = n;
-		num_to_char(un);
-	}
-}
-
-/**
  * num_to_char - transforms a number with 1 or more digits into a char
  * @n: number to print
  *
